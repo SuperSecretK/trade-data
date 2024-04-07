e11ca1a8810d11b7f8bc31f8542c345b4da2c434
MagusX <brianddoan@gmail.com>
2024-04-08 01:51:56 +0700

    Init

---
+++ index.js
@@ -0,0 +1,76 @@
+const axios = require('axios');
+
+const parseHistory = async (data) => {
+    // let date = [];
+    // await data['t'].forEach(d => {
+    //   date.push(new Date(d * 1000));
+    // });
+
+    // return {
+    //   closeList: data['c'],
+    //   openList: data['o'],
+    //   highList: data['h'],
+    //   lowList: data['l'],
+    //   dateList: date
+    // }
+
+    let res = [];
+    await data['t'].forEach((d, index) => {
+        res.push({
+            'date': new Date(d * 1000),
+            'price': data['c'][index]
+        });
+    });
+
+    return res;
+  }
+
+const _getRoundData = async (symbol, onDate, backRange) => {
+    const today = new Date(onDate);
+    const past = new Date(today);
+    const from = Math.floor(past.setDate(today.getDate() - parseInt(backRange, 10)) / 1000);
+    const to = Math.floor(today.getTime() / 1000);
+  
+    // const url = `https://tvc4.investing.com/78675672b5d8b9dd3a47b561a9410d0b/1712503480/52/52/110/history?symbol=41063&resolution=D&from=${from}&to=${to}`
+    const url = `https://dchart-api.vndirect.com.vn/dchart/history?resolution=D&symbol=${symbol}&from=${from}&to=${to}`;
+    console.log(url);
+    const response = await axios.get(url);
+    return {
+      past: past.toLocaleDateString(),
+      response: response.data
+    };
+  }
+  
+const _getPriceHistory = async (symbol, date, days) => {
+    let result = [];
+    let rounds;
+    const rest = days % 1000;
+    let range = 1000
+    if (days < 1000) {
+      rounds = 1;
+      range = days;
+    } else {
+      rounds = Math.floor(days / 1000);
+    }
+  
+    let today = date === 'today' ? new Date() : new Date(date);
+    for (let i = 0; i < rounds; i++) {
+      const { past, response } = await _getRoundData(symbol, today, range);
+      console.log(`${today.toLocaleDateString()} to ${past}`);
+      const res = await parseHistory(response);
+      result = [...res, ...result];
+  
+      today = new Date(past);
+      today.setDate(today.getDate() - 1);
+  
+      if (range !== rest && days > 1000 && i === rounds - 1) {
+        range = rest;
+        rounds++;
+      }
+    }
+  
+    console.log(result);
+    return result;
+}
+
+_getPriceHistory('VNINDEX', '2017-08-01', 1000);
